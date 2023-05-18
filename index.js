38c866b2a08802826da5cd9d3886640d39bb36f8
Maksym Lukianenko <maks.lukyan@gmail.com>
2023-05-18 23:32:58 +0300

    add task

--- index.js
+++ index.js
@@ -39,8 +39,4 @@ async function invokeAction({ action, id, name, email, phone }) {
       console.warn('\x1B[31m Unknown action type!');
   }
 }
-// invokeAction({
-//   action: 'remove',
-//   id: 'KztDeYsHFw4J4hpKoUdpu',
-// });
 invokeAction(argv);
